c343f9ce76d8f01ba547d34690657307a4382e56
Mr.Web <lombardi.nd@gmail.com>
2022-11-19 00:38:11 +0100

    Test offline

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ciao! Da <a href="https://nextjs.org">Next.js!</a>
+          Ciao! Benvenuto in <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
